46e85506e57c18f84c2569d42a12f042e13a3a0d
Lvv.me <cntrump@gmail.com>
2022-04-20 13:46:25 +0800

    Remove duplicated defines

--- build.sh
+++ build.sh
@@ -4,36 +4,35 @@ set -eux
 
 sqlite_src=sqlite-autoconf-3380200
 
-if [ ! -d ${sqlite_src} ]; then
-  tar xvf ${sqlite_src}.tar.xz
-fi
+[ -d ${sqlite_src} ] && rm -rf ${sqlite_src}
+
+tar xvf ${sqlite_src}.tar.xz
 
 pushd ${sqlite_src}
 
+threading_mode=2
+sed -i -e "s/-DSQLITE_THREADSAFE=1/-DSQLITE_THREADSAFE=${threading_mode}/g" ./configure
+
 features=(
-    "-DSQLITE_THREADSAFE=1"
-    "-DSQLITE_ENABLE_EXPLAIN_COMMENTS"
-    "-DSQLITE_HAVE_ZLIB"
     "-DSQLITE_INTROSPECTION_PRAGMAS"
     "-DSQLITE_ENABLE_UNKNOWN_SQL_FUNCTION"
-    "-DSQLITE_ENABLE_STMTVTAB"
-    "-DSQLITE_ENABLE_DBPAGE_VTAB"
-    "-DSQLITE_ENABLE_DBSTAT_VTAB"
     "-DSQLITE_ENABLE_OFFSET_SQL_FUNC"
     "-DSQLITE_ENABLE_JSON1"
-    "-DSQLITE_ENABLE_RTREE"
-    "-DSQLITE_ENABLE_FTS4"
-    "-DSQLITE_ENABLE_FTS5"
 )
 
 CC=clang \
 CFLAGS="-target apple-macosx10.9 -arch arm64 -arch x86_64 ${features[*]}" \
-./configure --prefix=/opt/local \
+./configure --prefix="$(pwd)/../local" \
             --enable-static-shell \
             --enable-editline \
-            --enable-readline
+            --enable-readline \
+            --enable-threadsafe
 
 make -j
 sudo make install
 
+popd
+
+pushd "local"
+tar --uid 0 --gid 0 -cJvf ../sqlite3.tar.xz $(ls)
 popd
